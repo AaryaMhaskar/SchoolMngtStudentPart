c9d92e30bb8926b7ab02bd432e96d66ef1f7cf25
Admin <Admin@DESKTOP-AUJV498>
2024-09-12 17:40:48 +0530

    School Student Part

--- SchoolManagementUsingWebApi/Controllers/StudentController.cs
+++ SchoolManagementUsingWebApi/Controllers/StudentController.cs
@@ -18,6 +18,7 @@ namespace SchoolManagementUsingWebApi.Controllers
         [HttpPost]
         public IActionResult AddStudent(Student student)
         {
+            student.FeesStatus = "Pending";
             db.Students.Add(student);
             db.SaveChanges();
             return Ok("student added sucessfully!!");
@@ -53,6 +54,81 @@ namespace SchoolManagementUsingWebApi.Controllers
         {
             var d = db.Attendances.ToList();
             return Ok(d);
+        }
+
+        [Route("GetFeeStatus")]
+        [HttpGet]
+        public IActionResult GetFeeStatus()
+        {
+            var d = db.Students.ToList();
+            return Ok(d);
+        }
+
+        [Route("GetStudentProfile")]
+        [HttpGet]
+        public IActionResult GetStudentProfile()
+        {
+            var d = db.Students.ToList();
+            return Ok(d);
+        }
+
+
+
+
+
+        [Route("GetStudentById/{id}")]
+        [HttpGet]
+        public IActionResult GetStudentById( int id)
+        {
+            var d = db.Students.Where(x => x.StudentId.Equals(id)).SingleOrDefault();
+            return Ok(d);
+        }
+
+
+
+
+
+
+
+        [Route("UpdateStudent")]
+        [HttpPut]
+        public IActionResult UpdateStudent([FromBody] Student s)
+        {
+            if (s == null || s.StudentId <= 0)
+            {
+                return BadRequest("Invalid student data.");
+            }
+
+            var student = db.Students.SingleOrDefault(st => st.StudentId == s.StudentId);
+            if (student == null)
+            {
+                return NotFound("Student not found.");
+            }
+
+            // Update student properties
+            student.FirstName = s.FirstName;
+            student.LastName = s.LastName;
+            student.PhoneNumber = s.PhoneNumber;
+
+            // Update other fields as necessary
+
+            db.SaveChanges();
+            return Ok("Student Updated Successfully");
+        }
+
+        //ddfjfh
+        [Route("UploadFile")]
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile e)
+        {
+            var filepath = "Images/" + e.FileName;
+            FileStream stream = new FileStream(filepath,FileMode.Create);
+            e.CopyTo(stream);
+            return Ok(filepath);
+
+
+
+
         }
     }
 }
